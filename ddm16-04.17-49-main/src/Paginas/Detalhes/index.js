3bcacded310b8205bfb5e8c8cda4887a9801adc2
unknown <pedronognogsilva@gmail.com>
2025-05-10 11:16:33 -0300

    first commit

---
+++ ddm16-04.17-49-main/src/Paginas/Detalhes/index.js
@@ -0,0 +1,14 @@
+import { View, Text ,TouchableOpacity} from "react-native-web";
+import { useNavigation } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+export default function Detalhes(){
+    const route = useRoute();
+    const navigation = useNavigation();
+    return(
+        <View>
+            <Text> {route.params.titulo}</Text>
+            <Text> {route.params.nota}</Text>
+            <TouchableOpacity onPress={()=> navigation.goBack()}> Clique aqui para voltar </TouchableOpacity>
+        </View>
+    )
+}
