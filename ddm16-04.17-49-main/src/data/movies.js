43605173028c9da4b433a14242bf5d0b7ec1cfc7
PedroNogNog <pedronognogsilva@gmail.com>
2025-05-20 16:21:53 -0300

    Update movies.js

--- ddm16-04.17-49-main/src/data/movies.js
+++ ddm16-04.17-49-main/src/data/movies.js
@@ -47,6 +47,76 @@ const DATA = [
         imagem:'https://br.web.img3.acsta.net/c_310_420/pictures/16/02/03/19/11/303307.jpg'
 
     },
+    {
+        id:'08',
+        nome:'Thunderbolts*',
+        nota:'4,3',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/img/1d/6f/1d6f98938e13ae86e4178850a709b9f3.png'
+
+    },
+    {
+        id:'09',
+        nome:'Operação Big Hero',
+        nota:'4,5',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/pictures/14/10/17/19/18/097729.jpg'
+
+    },
+    {
+        id:'10',
+        nome:'O Poderoso Chefão',
+        nota:'4,8',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/93/20/20120876.jpg'
+
+    },
+    {
+        id:'11',
+        nome:'Karate Kid: Lendas',
+        nota:'3,9',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/img/6a/9e/6a9edfd55ce28ca6df541b983c2d9d25.jpg'
+
+    },
+    {
+        id:'12',
+        nome:'Um Filme Minecraft',
+        nota:'3,7',
+        imagem:'https://br.web.img2.acsta.net/c_310_420/img/39/68/3968d0b052682d5c6625be54c9b513ef.png'
+
+    },
+    {
+        id:'13',
+        nome:'Batman - O Cavaleiro Das Trevas',
+        nota:'4,8',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/86/98/32/19870786.jpg'
+
+    },
+    {
+        id:'14',
+        nome:'Vingadores: Ultimato',
+        nota:'4,7',
+        imagem:'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg'
+
+    },
+    {
+        id:'15',
+        nome:'Bastardos Inglórios',
+        nota:'4,6',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/43/36/20096333.jpg'
+
+    },
+    {
+        id:'16',
+        nome:'O Macaco',
+        nota:'2,5',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/img/d2/e5/d2e517612de9e1db2463aeec34dc6d29.jpg'
+
+    },
+    {
+        id:'17',
+        nome:'Planeta dos Macacos: A Guerra',
+        nota:'4,3',
+        imagem:'https://br.web.img3.acsta.net/c_310_420/pictures/17/07/13/18/50/359962.png'
+
+    },
 ]
 
 export default DATA;
