aa7fd43b5dcc3430a42aa28a165fe0adc85cf1d4
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-05-21 18:08:20 -0300

    Update index.js
    borra porra

--- ddm16-04.17-49-main/src/Paginas/Detalhes/index.js
+++ ddm16-04.17-49-main/src/Paginas/Detalhes/index.js
@@ -1,18 +1,24 @@
-import { View, Text ,TouchableOpacity} from "react-native-web";
-import { useNavigation } from "@react-navigation/native";
-import { useRoute } from "@react-navigation/native";
-export default function Detalhes(){
+import React from "react";
+import styles from './style'; 
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native-web";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+export default function Detalhes() {
     const route = useRoute();
     const navigation = useNavigation();
-    return(
-        <View>
-            <Text> {route.params.titulo}</Text>
-            <Text> {route.params.nota}</Text>
-      <Image
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{route.params.titulo}</Text>
+            <Text style={styles.note}>Nota: {route.params.nota}</Text>
+            <Image
                 source={{ uri: route.params.imagem }}
                 style={styles.image}
             />
-            <TouchableOpacity onPress={()=> navigation.goBack()}> Clique aqui para voltar </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button}>
+                <Text style={styles.buttonText}>Clique aqui para voltar</Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
+
