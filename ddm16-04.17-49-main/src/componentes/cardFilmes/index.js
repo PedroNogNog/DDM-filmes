c7be717ae4b784e2a001820ac315aaaaf07e851b
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-05-23 13:53:13 -0300

    Update index.js

--- ddm16-04.17-49-main/src/componentes/cardFilmes/index.js
+++ ddm16-04.17-49-main/src/componentes/cardFilmes/index.js
@@ -2,12 +2,30 @@ import React from 'react'
 import {View, Image, Text, TouchableOpacity} from 'react-native'; 
 import styles from './style'; 
 import { useNavigation } from '@react-navigation/native';
-export default function CardMovies({titulo,imagem,nota}){ 
+export default function CardMovies({titulo,imagem,nota,sinopse}){ 
 
     const navigation = useNavigation();
 return( 
 
-    <TouchableOpacity style={styles.containerJogos} onPress={()=> navigation.navigate('Detalhes',{titulo,nota,imagem})} > 
+    <TouchableOpacity style={styles.containerJogos} onPress={()=> navigation.navigate('Detalhes',{titulo,nota,imagem,sinopse})} > 
+    
+<Image style = {styles.images} source = {{uri:(imagem)}} /> 
+<Text style ={styles.titulo}>{titulo}</Text> 
+<Text style = {styles.textNota}>{nota}</Text> 
+</TouchableOpacity> 
+
+); 
+}
+import React from 'react' 
+import {View, Image, Text, TouchableOpacity} from 'react-native'; 
+import styles from './style'; 
+import { useNavigation } from '@react-navigation/native';
+export default function CardMovies({titulo,imagem,nota,sinopse}){ 
+
+    const navigation = useNavigation();
+return( 
+
+    <TouchableOpacity style={styles.containerJogos} onPress={()=> navigation.navigate('Detalhes',{titulo,nota,imagem,sinopse})} > 
     
 <Image style = {styles.images} source = {{uri:(imagem)}} /> 
 <Text style ={styles.titulo}>{titulo}</Text> 
