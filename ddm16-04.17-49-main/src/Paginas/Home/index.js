2ca0a96081a88fa0d715a5c6167a4c9f284e245a
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-08-19 16:30:45 -0300

    Update index.js

--- ddm16-04.17-49-main/src/Paginas/Home/index.js
+++ ddm16-04.17-49-main/src/Paginas/Home/index.js
@@ -1,63 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, FlatList } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-
-import DATA from '../../../src/data/movies.js'
-
-import Rotas from '../../rotas/index.js';
-
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
 import Cabecalho from '../../componentes/cabecalho';
-import Pesquisa from '../../componentes/pesquisa/index.js';
-import Banners from '../../componentes/banner/index.js';
-import CardMovies from '../../componentes/cardFilmes/index.js';
-import { useState, useEffect } from 'react';
-
+import Pesquisa from '../../componentes/pesquisa';
+import Banners from '../../componentes/banner';
+import CardMovies from '../../componentes/cardFilmes';
 
 export default function App() {
-  const imagem = Math.floor(Math.random() * 4 + 1);
+  const [movies, setMovies] = useState([]);
 
-const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function buscarFilmes() {
+      const url = 'https://api.themoviedb.org/3/movie/top_rated?language=pt-br&page=1';
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer YOUR_API_KEY',
+        },
+      };
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data.results);
+      setMovies(data.results);  
+    }
 
-useEffect(()=>{
-
-  async function buscarFilmes() {
-    
-    const url = 'https://api.themoviedb.org/3/movie/changes?page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDE1YjczMTBjZmI3NTFmOTdhYWFjNjlmZDI2ZGFhNiIsIm5iZiI6MTc1NTAyMTU2Ny4xMjYwMDAyLCJzdWIiOiI2ODliODBmZmI0Njc5ZDQzOTMxYTMyOTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.RW8QpCYzq7VeUfG6uoRI8IzufbY9VHRcipUd4oCoOvw'
-  
-  }
-  }
-
-  
-};
-})
-
-  
-
-
+    buscarFilmes();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Cabecalho />
-      
       <Pesquisa />
-
       <Banners />
-
       <View style={{ width: '90%' }}>
         <FlatList
-          data={DATA}
-          horizontal={true}
-          keyExtractor={(item) => item.id}
+          data={movies}
+          horizontal
+          keyExtractor={(item) => item.id.toString()} 
           renderItem={({ item }) => (
             <CardMovies
               titulo={item.title}
-              nota={item.nota}
-              imagem={item.vote.path}
+              nota={item.vote_average}
+              imagem={`https://image.tmdb.org/t/p/w500${item.poster_path}`}  
+              sinopse={item.overview}
             />
           )}
         />
@@ -70,7 +55,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#141a29',
-    alignItems: "center"
-  }
-
+    alignItems: 'center',
+  },
 });
