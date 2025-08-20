cf32c9e4085dbcaaccb278d12d4aa0be516b3c79
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-08-20 13:34:55 -0300

    Update index.js

--- ddm16-04.17-49-main/src/Paginas/Home/index.js
+++ ddm16-04.17-49-main/src/Paginas/Home/index.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import Cabecalho from '../../componentes/cabecalho';
 import Pesquisa from '../../componentes/pesquisa';
 import Banners from '../../componentes/banner';
 import CardMovies from '../../componentes/cardFilmes';
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDE1YjczMTBjZmI3NTFmOTdhYWFjNjlmZDI2ZGFhNiIsIm5iZiI6MTc1NTAyMTU2Ny4xMjYwMDAyLCJzdWIiOiI2ODliODBmZmI0Njc5ZDQzOTMxYTMyOTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.RW8QpCYzq7VeUfG6uoRI8IzufbY9VHRcipUd4oCoOvw',
+      },
+    };
+
     async function buscarFilmes() {
-      const url = 'https://api.themoviedb.org/3/movie/top_rated?language=pt-br&page=1';
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer YOUR_API_KEY',
-        },
-      };
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data.results);
-      setMovies(data.results);  
+      try {
+        const responseTopRated = await fetch(
+          'https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1',
+          options
+        );
+        const dataTopRated = await responseTopRated.json();
+        setTopRatedMovies(dataTopRated.results);
+
+        const responsePopular = await fetch(
+          'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1',
+          options
+        );
+        const dataPopular = await responsePopular.json();
+        setPopularMovies(dataPopular.results);
+      } catch (error) {
+        console.error('Erro ao buscar filmes:', error);
+      }
     }
 
     buscarFilmes();
@@ -32,16 +46,35 @@ export default function App() {
       <Cabecalho />
       <Pesquisa />
       <Banners />
-      <View style={{ width: '90%' }}>
+
+      <View style={styles.listaContainer}>
+        <Text style={styles.tituloLista}>Mais bem avaliados</Text>
         <FlatList
-          data={movies}
+          data={topRatedMovies}
           horizontal
-          keyExtractor={(item) => item.id.toString()} 
+          keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <CardMovies
               titulo={item.title}
               nota={item.vote_average}
-              imagem={`https://image.tmdb.org/t/p/w500${item.poster_path}`}  
+              imagem={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+              sinopse={item.overview}
+            />
+          )}
+        />
+      </View>
+
+      <View style={styles.listaContainer}>
+        <Text style={styles.tituloLista}>Populares</Text>
+        <FlatList
+          data={popularMovies}
+          horizontal
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <CardMovies
+              titulo={item.title}
+              nota={item.vote_average}
+              imagem={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
               sinopse={item.overview}
             />
           )}
@@ -56,5 +89,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#141a29',
     alignItems: 'center',
+  },
+  listaContainer: {
+    width: '90%',
+    marginTop: 20,
+  },
+  tituloLista: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
